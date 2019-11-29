92a484ab4f4d059ba6f919ad9032753e22cf7f7c
dovanhanh1997 <dovanhanh12b@gmail.com>
2019-11-29 15:52:54 +0700

    ..

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello Hanh
+                    Hello Hang
                 </div>
 
                 <div class="links">
